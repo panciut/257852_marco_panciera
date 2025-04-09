46bb103666a9942b8c1b753867b339737a4793f8
panciut <pancimarco02@gmail.com>
2025-04-09 17:34:01 +0200

    model directory fix

--- LM/part_A/main.py
+++ LM/part_A/main.py
@@ -69,5 +69,5 @@ if __name__ == "__main__":
     test_ppl, _ = eval_loop(test_loader, criterion_eval, best_model)
     print(f"Final test PPL: {test_ppl:.2f}")
     # Save the best model
-    torch.save(best_model.state_dict(), "../../models/baseline_rnn.pt")
-    print("Model saved to: ../../models/baseline_rnn.pt")
+    torch.save(best_model.state_dict(), "models/baseline_rnn.pt")
+    print("Model saved to: models/baseline_rnn.pt")
