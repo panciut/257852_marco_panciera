49fd9e60fb2ca5317ecf057b087b5f9b17dba085
panciut <pancimarco02@gmail.com>
2025-04-11 18:23:22 +0200

    2 e 3

--- LM/part_A/run_all_experiments.py
+++ LM/part_A/run_all_experiments.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 
 # List of experiment IDs to run
-EXPERIMENT_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+EXPERIMENT_IDS = [2, 3]
 
 # Path to main script
 MAIN_SCRIPT = "LM/part_A/main.py"
